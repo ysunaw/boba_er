21ed8b35ca5642b6b736c6277bb00120d110812e
ysunaw <ysunaw@gmail.com>
2022-03-22 23:20:41 -0400

    added navbar and react dom

--- boba_er/src/App.js
+++ boba_er/src/App.js
@@ -1,38 +1,44 @@
 import React from "react"; 
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import { Button, Navbar } from 'react-bulma-components';
+import Home from './pages/Home';
+import Journal from './pages/Journal';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+
 
 function App() {
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("http://localhost:9000/testapi")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .then((data) => setData(data.message));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("http://localhost:9000/testapi")
+  //     .then((res) => res.text())
+  //     .then((res) => this.setState({ apiResponse: res }))
+  //     .then((data) => setData(data.message));
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          
-        </p>
-        <p>{!data ? "Loading..." : data}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar>
+        <Navbar.Brand>BOBAER</Navbar.Brand>
+          <Navbar.Item href="Journal">JOURNAL</Navbar.Item>
+          <Navbar.Item href="Recommenation">RECOMMENDATION</Navbar.Item>
+          <Navbar.Item href="Blogs">ABOUT</Navbar.Item>
+        <Navbar.Container align="right">
+          <Button>LOG IN OR SIGN UP</Button>
+        </Navbar.Container>
+        </Navbar>
+        <Routes>
+            <Route exact path='/' element = {<Home />} />
+            <Route path='/Journal' element={<Journal />} />
+
+        </Routes>
+    </Router>
   );
 }
 
 
 
 export default App;
+
---
+++ boba_er/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react"; 
+import 'bulma/css/bulma.min.css';
+import { Button, Navbar } from 'react-bulma-components';
+
+const Home = () => {
+    return (
+    <div className="Home">
+      <div className="Main">
+          <p>BLOGS</p>
+      </div>
+    </div>
+    );
+};
+
+
+
+export default Home;
+
